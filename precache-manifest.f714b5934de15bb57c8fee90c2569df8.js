4c150cc34232bdfd1dfada27c43ac2bab511df4d
danRalph <dan.r.richards@hotmail.co.uk>
2020-08-18 13:46:59 +0100

    Updates

---
+++ precache-manifest.f714b5934de15bb57c8fee90c2569df8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef6f4404f07768fbb359f6c2a82464a",
+    "url": "/filmSearch-productionBuild/index.html"
+  },
+  {
+    "revision": "1e59ff9291c1800db6f0",
+    "url": "/filmSearch-productionBuild/static/css/main.c4e4ed2c.chunk.css"
+  },
+  {
+    "revision": "d109ad2e54ceaeb85918",
+    "url": "/filmSearch-productionBuild/static/js/2.0733b272.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/filmSearch-productionBuild/static/js/2.0733b272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e59ff9291c1800db6f0",
+    "url": "/filmSearch-productionBuild/static/js/main.701f3c3c.chunk.js"
+  },
+  {
+    "revision": "6a437ab13b8ff5555ecd",
+    "url": "/filmSearch-productionBuild/static/js/runtime-main.6917cb7d.js"
+  }
+]);
